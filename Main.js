695d32b87df7e9d5df4e2ce0822b46bca07da829
HellTemplar <revolution837@yahoo.com>
2015-02-04 13:29:53 -0800

    Funny npc
    still trying to make npc stand still but console kept being angry and
    says "(something) is undefined" everytime.

--- Main.js
+++ Main.js
@@ -3,6 +3,8 @@ var ASSET_MANAGER = new AssetManager();
 //ASSET_MANAGER.queueDownload("./imgs/hero_hooded.png");
 //ASSET_MANAGER.queueDownload("./imgs/skeleton.png");
 ASSET_MANAGER.queueDownload("./imgs/warrior.png")
+ASSET_MANAGER.queueDownload("./imgs/npc-female.png")
+
 
 ASSET_MANAGER.downloadAll(function () {
     var canvas = document.getElementById("gameworld");
@@ -11,10 +13,12 @@ ASSET_MANAGER.downloadAll(function () {
     var gameEngine = new GameEngine();
 
     var warrior = new Warrior(gameEngine);
+    var npc = new NPC(gameEngine);
 
     gameEngine.addEntity(warrior);
+    gameEngine.addEntity(npc);
+
     //gameEngine.addEntity(new Enemy(gameEngine, ASSET_MANAGER.getAsset("./imgs/skeleton.png")));
     gameEngine.init(context);
     gameEngine.start();
-    //asdfasdfasdf
 });
