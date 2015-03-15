7ea5e27a8cfd9db30318372f3efb8f6594ddfb20
HellTemplar <revolution837@yahoo.com>
2015-03-15 02:08:43 -0700

    Stuff
    Level 2 music renamed abit, coordinate changes AND 2k hero attack for
    testing purposes

--- Main.js
+++ Main.js
@@ -76,7 +76,7 @@ ASSET_MANAGER.downloadAll(function () {
 	var mountain_villager_spritesheet = ASSET_MANAGER.getAsset("./imgs/mountainVillager.png");
 
 
-	var warrior = new Warrior(gameEngine, new Statistics(300, 22, 35, 4, 3, 1));
+	var warrior = new Warrior(gameEngine, new Statistics(300, 2200, 35, 4, 3, 1));
 	var archer = new Archer(gameEngine, new Statistics(225, 25, 28, 4, 3, 1));
     gameEngine.heroes.push(warrior);
     gameEngine.heroes.push(archer);
