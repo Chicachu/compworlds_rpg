721c62659b1e2a9dc2a549652834ab1c137e0774
tpriymak <tim_igoravich@yahoo.com>
2015-03-15 01:30:29 -0700

    changed x back to 320

--- GameEngine.js
+++ GameEngine.js
@@ -1562,7 +1562,7 @@ Warrior = function (game, stats) {
         special: new Animation(this.spriteSheet, 0, 17, 64, 64, 0.05, 12, true, false),
         death: new Animation(this.spriteSheet, 0, 21, 64, 64, 0.5, 1, true, false)
     };
-    this.x = 220;
+    this.x = 320;
     this.y = 208;
 
     this.quests = [];
@@ -1903,7 +1903,6 @@ Dragon1.prototype.draw = function (context) {
     this.drawHealthBar(context);
     if (this.is_targeted) {
         this.drawSelector(context, 'yellow');
-
     }
     this.curr_anim.drawFrame(this.game.clockTick, context, this.x, this.y, 3);
 }
